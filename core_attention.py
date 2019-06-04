891ad20773479de9cb2ab64b0d976e03e81c91a7
Chung-Yeon LEE <jamixlee@gmail.com>
2019-06-04 18:12:10 +0900

    roll back the dimension size

--- core_attention.py
+++ core_attention.py
@@ -8,9 +8,9 @@ class InferenceCore(nn.Module):
         super(InferenceCore, self).__init__()
         self.downsample_x = nn.Conv2d(3, 3, kernel_size=4, stride=4, padding=0, bias=False)
         self.upsample_v = nn.ConvTranspose2d(7, 7, kernel_size=16, stride=16, padding=0, bias=False)
-        self.upsample_r = nn.ConvTranspose2d(256, 256, kernel_size=16, stride=16, padding=0, bias=False)
+        self.upsample_r = nn.ConvTranspose2d(128, 128, kernel_size=16, stride=16, padding=0, bias=False)
         self.downsample_u = nn.Conv2d(128, 128, kernel_size=4, stride=4, padding=0, bias=False)
-        self.core = Conv2dLSTMCell(3+7+256+2*128, 128, kernel_size=5, stride=1, padding=2)
+        self.core = Conv2dLSTMCell(3+7+128+2*128, 128, kernel_size=5, stride=1, padding=2)
         
     def forward(self, x, v, r, c_e, h_e, h_g, u):
         x = self.downsample_x(x)
@@ -26,8 +26,8 @@ class GenerationCore(nn.Module):
     def __init__(self):
         super(GenerationCore, self).__init__()
         self.upsample_v = nn.ConvTranspose2d(7, 7, kernel_size=16, stride=16, padding=0, bias=False)
-        self.upsample_r = nn.ConvTranspose2d(256, 256, kernel_size=16, stride=16, padding=0, bias=False)
-        self.core = Conv2dLSTMCell(7+256+3, 128, kernel_size=5, stride=1, padding=2)
+        self.upsample_r = nn.ConvTranspose2d(128, 128, kernel_size=16, stride=16, padding=0, bias=False)
+        self.core = Conv2dLSTMCell(7+128+3, 128, kernel_size=5, stride=1, padding=2)
         self.upsample_h = nn.ConvTranspose2d(128, 128, kernel_size=4, stride=4, padding=0, bias=False)
         
     def forward(self, v, r, c_g, h_g, u, z):
